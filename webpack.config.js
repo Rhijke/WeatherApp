9cb9fa974fb774f30978b7c185ad773cd4c0f746
Kaerah Lopez <kaerah.lopez@gmail.com>
2019-12-21 11:27:56 -0600

    Add css

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,14 @@
 const path = require('path');
 
 module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader']
+      }
+    ]
+  },
   entry: './src/app.js',
   output: {
     filename: 'app.js',
